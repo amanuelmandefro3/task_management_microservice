c78fde80970a2bec45f5f0116ac16d2ec4d02dc0
uchuulaa <elijahbag456@gmail.com>
2025-01-24 14:59:43 +0300

    notifications view and serializers

--- notification_service/notifications/serializers.py
+++ notification_service/notifications/serializers.py
@@ -1,1 +1,7 @@
-# elias
+from rest_framework import serializers
+from .models import NotificationLog
+
+class NotificationLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = NotificationLog
+        fields = '__all__'
