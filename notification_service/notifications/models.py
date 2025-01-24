291cc6e8cf25fe208613a3ca52ba2c4aa9043a49
Ahmed-AAY <162641927+AhmedA-AY@users.noreply.github.com>
2025-01-24 18:53:01 +0300

    add notification model and consumer

--- notification_service/notifications/models.py
+++ notification_service/notifications/models.py
@@ -1,1 +1,9 @@
-# ahmad
+from django.db import models
+
+class NotificationLog(models.Model):
+    event_type = models.CharField(max_length=100)
+    payload = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.event_type} - {self.created_at}"
