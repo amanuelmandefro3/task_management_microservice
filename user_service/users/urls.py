e55130d927c24afa98f8ebca869596fd65c6808a
adane <adanemoges6@gmail.com>
2025-01-24 12:53:25 +0000

    impliment user view

--- user_service/users/urls.py
+++ user_service/users/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet
+from .views import (
+    UserRegistrationView,
+    EmailVerificationView,
+    UserLoginView,
+    UserDetailView,
+    UserUpdateView,
+    PasswordChangeView,
+    ForgotPasswordView,
+    ResetPasswordView,
+)
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet, basename='users')
-
-urlpatterns = router.urls
+urlpatterns = [
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('verify-email/', EmailVerificationView.as_view(), name='verify-email'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('me/update/', UserUpdateView.as_view(), name='user-update'),
+    path('me/change-password/', PasswordChangeView.as_view(), name='change-password'),
+    path('forgot-password/', ForgotPasswordView.as_view(), name='forgot-password'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
+]
