e53129b62b0e8a9167211fad77f774206f86e6ed
Ahmed-AAY <123978727+akremAhmed23@users.noreply.github.com>
2025-01-24 19:12:54 +0300

    add task model and rabbitmq_utils

--- task_service/tasks/models.py
+++ task_service/tasks/models.py
@@ -1,1 +1,11 @@
-# akrem
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    assigned_user_id = models.IntegerField(null=True, blank=True)
+    status = models.CharField(max_length=50, default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.status}"
